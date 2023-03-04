f11852ccf737d8050aecc67ecdba14e5f96ccae0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-04 19:14:36 +0530

    Interpolation and Data Binding

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -12,6 +12,9 @@ Vue.createApp({
         }
     }
 }).mount('#app');
+
+// pure js code for above solution
+
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
