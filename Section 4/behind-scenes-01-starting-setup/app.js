fb8c49786ac0238753ac369a5d3e57056661cee6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-09 21:45:52 +0530

    Understanding Templates

--- Section 4/behind-scenes-01-starting-setup/app.js
+++ Section 4/behind-scenes-01-starting-setup/app.js
@@ -18,6 +18,7 @@ const app = Vue.createApp({
 app.mount('#app');
 
 const app2 = Vue.createApp({
+  template:`<p>{{favouriteMeal}}</p>`,
       data(){
         return{
           favouriteMeal:'Pizza'
