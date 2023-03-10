03b1713877f44e0a307a637321e2b211a4c77845
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-10 14:52:33 +0530

    Introducing Components

--- Section 5/cmp-intro-01-starting-setup/app.js
+++ Section 5/cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return{
-            detailsAreVisible:false,
             friends:[
                 {id: 'manuel',
                  name:'Manuel Lorenz', 
@@ -15,13 +14,36 @@ const app = Vue.createApp({
                },
             ]
         }
+    }
+});
+
+app.component('friend-contact',{
+    template:`
+    <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails"> {{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+  </li>
+    `,
+    data(){
+        return {
+            detailsAreVisible:false,
+            friend: {
+                id: 'manuel',
+                name:'Manuel Lorenz', 
+                phone:'0713725452',
+                email:'dulanjali@gmail.com'   
+            }
+        };
     },
     methods:{
         toggleDetails(){
             this.detailsAreVisible = !this.detailsAreVisible;
-        }
+        } 
     }
-    
 });
 
 app.mount("#app");
