9365f2f9d02787257611068e5fe3bd572de970a5
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-12 13:53:00 +0530

    Creating a Basic Vue App

---
+++ my_app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>My Friends</h2>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+  export default{
+    data(){
+    return {
+            friends:[
+               {
+                id:'manuel',
+                name: 'Manuel Lorenz',
+                phone:'0713724587',
+                email:'dula@gmail.com'
+               },
+               {
+                id:'dula',
+                name: 'Dulanjali Senarathna',
+                phone:'0713724583',
+                email:'dula93@gmail.com'
+               }
+            ]
+        }
+    },
+  }
+</script>
