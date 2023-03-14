dc038f0d01b80bfbc9f5ae2be181ce75aa2ed776
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-14 10:32:48 +0530

    Assignment 6 done

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @set-data="setUserData"></user-data>
+    </div>
+</template>
+
+<script>
+export default {
+   data(){
+    return{
+        user:{
+            name: 'Dulanjali ',
+            age:31
+        }
+    }
+   },
+   methods:{
+    setUserData(name,age){
+        this.user ={
+            name : name,
+            age : +age //convert string to a number
+        }
+    }
+   } 
+}
+</script>
+
+
